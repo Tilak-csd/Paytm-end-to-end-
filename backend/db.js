f5ec4c07069b8b51022230dc62a7bb26b354d1ec
Tilak-csd <tilakgubhaju3@gmail.com>
2025-12-02 12:22:35 +0545

    initilazing the front end files and folder

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,18 @@
 const mongoose = require("mongoose")
+import dotenv from 'dotenv';
+dotenv.config()
 
-const UserSchema = mongoose.Schema({
-    firstname : String,
-    lastname  : String,
-    email : String,
-    password : String,
-    phonenumber : Number
+const db_link = process.env.MONGOOSE_SERVER
+mongoose.connect(db_link)
+
+const UserSchema = new Schema({
+    firstname: String,
+    lastname: String,
+    email: String,
+    password: String,
+    phonenumber: Number
 })
 
 const User = mongoose.model("User", UserSchema)
 
-module.exports = {User}
+module.exports = { User }
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const app = express()
+const cors = require('cors')
 const userRoute = require('./routes/UserRoute')
-app.use(express.json())
 
+app.use(express.json())
+app.use(cors())
 
 app.use('/user', userRoute)
 
